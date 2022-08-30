a312f3c3ca126b3b82ed69f1624173641585e0fb
alfredo <fredo012@comunidad.unam.mx>
2022-08-30 18:11:02 -0500

    Ejemplofork() 1

---
+++ programas/tema_2/programa_00/fork.c
@@ -0,0 +1,9 @@
+#include <sys/types.h>
+#include <unistd.h>    // Para que reconozca fork()
+
+int main()
+{
+
+pid_t fork();          // No tiene argumentos y nos regresa un identificador de tareas
+
+}
